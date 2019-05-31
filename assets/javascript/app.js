9d1b53158f3ce95ef4557cf7b496c6573d82a035
Darryl Tolentino <darryl.tolentino@gmail.com>
2019-05-30 19:21:36 -0700

    Adding relation to answer section to question array in app.js so answers show based on question

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,11 +1,21 @@
-var timer = 15;  
+var timer = 15;
 var questionCount = 0;
+var questionAnswered = false;
 
 var correctAnswers = ["white", "sphere"];
 
 var questions = [
-    {q: "What color is the whiteboard?", a1: "blue", a2: "green", a3: "white", a4: "yellow"},
-    {q: "What shape is a basketball?", a1: "sphere", a2: "cube", a3: "cylinder", a4: "cone"}
+    { q: "What color is a whiteboard?", a1: "blue", a2: "green", a3: "white", a4: "yellow" },
+    { q: "What shape is a basketball?", a1: "sphere", a2: "cube", a3: "cylinder", a4: "cone" }
 ];
 
-$("#question").text("Question: " + questions[questionCount].q);
+function startUpTrivia() {
+    $("#question").text("Question: " + questions[questionCount].q);
+    $("#answer1").text(questions[questionCount].a1);
+    $("#answer2").text(questions[questionCount].a2);
+    $("#answer3").text(questions[questionCount].a3);
+    $("#answer4").text(questions[questionCount].a4);
+
+}
+
+startUpTrivia();
