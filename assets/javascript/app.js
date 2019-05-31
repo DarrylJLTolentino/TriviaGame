01bd023676b96fdc21184c2cc3af113be450b2bd
Darryl Tolentino <darryl.tolentino@gmail.com>
2019-05-30 17:42:36 -0700

    Adding code to display question on the dom in app.js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,9 +1,11 @@
 var timer = 15;  
+var questionCount = 0;
 
 var correctAnswers = ["white", "sphere"];
 
 var questions = [
     {q: "What color is the whiteboard?", a1: "blue", a2: "green", a3: "white", a4: "yellow"},
     {q: "What shape is a basketball?", a1: "sphere", a2: "cube", a3: "cylinder", a4: "cone"}
-]
+];
 
+$("#question").text("Question: " + questions[questionCount].q);
