89d8aa1a19e7ed06953a04b6349c698875968738
Darryl Tolentino <darryl.tolentino@gmail.com>
2019-05-30 17:22:30 -0700

    Adding an array of two question objects to app.js

---
+++ assets/javascript/app.js
@@ -0,0 +1,4 @@
+var questions = [
+    {q: "What color is the whiteboard?", a1: "blue", a2: "green", a3: "white", a4: "yellow"},
+    {q: "What shape is a basketball?", a1: "sphere", a2: "cube", a3: "cylinder", a4: "cone"}
+]
