517667f5be916de27f18abf7d57d83bd9d1dcaa4
Darryl Tolentino <darryl.tolentino@gmail.com>
2019-05-30 17:24:49 -0700

    Adding array of correct answers to compare to in app.js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,5 @@
+var correctAnswers = ["white", "sphere"];
+
 var questions = [
     {q: "What color is the whiteboard?", a1: "blue", a2: "green", a3: "white", a4: "yellow"},
     {q: "What shape is a basketball?", a1: "sphere", a2: "cube", a3: "cylinder", a4: "cone"}
