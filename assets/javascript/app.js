b0dd8c6a42704080844dc48a55320e73effb5d87
Darryl Tolentino <darryl.tolentino@gmail.com>
2019-06-02 01:29:19 -0700

    Adding images to assets. Adding four questions, four answers and their respective card ids and image names and removing game logic test questions within app.js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -6,12 +6,14 @@ var intervalVar;
 var answerValue;
 var questions = [
     { q: "What is the Attack Power of Blue-Eyes White Dragon", a1: "2500", a2: "3000", a3: "3500", a4: "3250" },
-    { q: "What color is a whiteboard?", a1: "blue", a2: "green", a3: "white", a4: "yellow" },
-    { q: "What shape is a basketball?", a1: "sphere", a2: "cube", a3: "cylinder", a4: "cone" }
+    { q: "What was the name of the puzzle Yugi Moto solved?", a1: "Millenium", a2: "Pyramid", a3: "Monolith", a4: "Egyptian" },
+    { q: "Answer Yugi's riddle: What is something you can show, but can't see?", a1: "Boundaries", a2: "Emotions", a3: "Gravity", a4: "Friendship" },
+    { q: "Which is Yugi Moto's most iconic monster card?", a1: "Summoned Skull", a2: "Celtic Guardian", a3: "Dark Magician", a4: "Kuriboh"},
+    { q: "What type of card is Stardust Dragon?", a1: "Synchro Monster", a2: "Magic Card", a3: "Trap Card", a4: "Fusion Monster"}
 ];
-var correctAnswers = ["3000","white", "sphere"];
-var imageArray = ["blue-eyes-white-dragon"];
-var idArray = [$("#card2")];
+var correctAnswers = ["3000", "Millenium", "Friendship", "Dark Magician", "Synchro Monster"];
+var imageArray = ["blue-eyes-white-dragon", "millenium-puzzle", "friendship", "dark-magician", "stardust-dragon"];
+var idArray = [$("#card2"), $("#card1"), $("#card4"), $("#card3"), $("#card1")];
 var answer1 = questions[questionCount].a1;
 var answer2 = questions[questionCount].a2;
 var answer3 = questions[questionCount].a3;
@@ -36,6 +38,7 @@ function startUpTrivia() {
     $("#card2").css("background-image", "none");
     $("#card3").css("background-image", "none");
     $("#card4").css("background-image", "none");
+    cardID = idArray[questionCount];
     timer = 6;
     startTimer();
     timeRemaining();
